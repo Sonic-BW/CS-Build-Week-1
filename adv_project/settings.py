0d8d7267cd19092bb6aa74a621b972236d76e166
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-01-16 00:33:52 -0600

    updated static file paths

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -153,8 +153,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR, 'static'),
+)
 
 import django_heroku
 django_heroku.settings(locals())
