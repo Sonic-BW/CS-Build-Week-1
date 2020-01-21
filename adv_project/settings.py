6f1cd5b58f0eac602e91c096a446f144daa25d42
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-01-20 22:52:05 -0600

    added adventure setting congig

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['sonicthelambhog.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'adventure.apps.AdventureConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
