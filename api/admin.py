649abc6e6b06b044e003915b61d0237e6f076b7c
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-01-25 15:03:55 -0600

    registered player model

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from adventure.models import Room
+from adventure.models import Room, Player
 
 # Register your models here.
 
 admin.site.register(Room)
+admin.site.register(Player)
+
