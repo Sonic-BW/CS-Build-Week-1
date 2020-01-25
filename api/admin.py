0c6d0196452a2dcb8155bb5016217fecfd31cd4c
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-01-25 16:33:53 -0600

    removed User register duplicate

--- api/admin.py
+++ api/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from adventure.models import Room, Player
-from django.contrib.auth.models import User
+
 
 # Register your models here.
 
 admin.site.register(Room)
 admin.site.register(Player)
-admin.site.register(User)
+
 
