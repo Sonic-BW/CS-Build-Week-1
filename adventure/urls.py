a814827048ef1804ce7a5b943a3e67b2dd4d6bf5
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-01-26 01:03:43 -0600

    added customized login response

--- adventure/urls.py
+++ adventure/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from . import api
 
+from .views import CustomObtainAuthToken
+
 urlpatterns = [
     url('init', api.initialize),
     url('move', api.move),
     url('say', api.say),
+    url(r'^authenticate/', CustomObtainAuthToken.as_view())
 ]
