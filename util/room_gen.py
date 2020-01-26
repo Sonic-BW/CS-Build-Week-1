bb08798293ef9995551ee4fd0a6dee75be972168
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-01-25 22:33:15 -0600

    added room gen for 100+

--- util/room_gen.py
+++ util/room_gen.py
@@ -1,231 +1,77 @@
-from django.db.models.signals import post_save
+# from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from adventure.models import Player, Room
 
 
-# Room.objects.all().delete()
-class Room:
-    def __init__(self, id, name, description, x, y):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.n_to = None
-        self.s_to = None
-        self.e_to = None
-        self.w_to = None
-        self.x = x
-        self.y = y
-    def __repr__(self):
-        if self.e_to is not None:
-            return f"({self.x}, {self.y}) -> ({self.e_to.x}, {self.e_to.y})"
-        return f"({self.x}, {self.y})"
-    def connect_rooms(self, connecting_room, direction):
-        '''
-        Connect two rooms in the given n/s/e/w direction
-        '''
-        reverse_dirs = {"n": "s", "s": "n", "e": "w", "w": "e"}
-        reverse_dir = reverse_dirs[direction]
-        setattr(self, f"{direction}_to", connecting_room)
-        setattr(connecting_room, f"{reverse_dir}_to", self)
-    def get_room_in_direction(self, direction):
-        '''
-        Connect two rooms in the given n/s/e/w direction
-        '''
-        return getattr(self, f"{direction}_to")
+Room.objects.all().delete()
 
-class World:
-    def __init__(self):
-        self.grid = None
-        self.width = 0
-        self.height = 0
-    def generate_rooms(self, size_x, size_y, num_rooms):
-        '''
-        Fill up the grid, bottom to top, in a zig-zag pattern
-        '''
-
-        # Initialize the grid
-        self.grid = [None] * size_y
-        self.width = size_x
-        self.height = size_y
-        for i in range( len(self.grid) ):
-            self.grid[i] = [None] * size_x
-
-        # Start from lower-left corner (0,0)
-        x = 1 # (this will become 0 on the first step)
-        y = 0
-        room_count = 0
-
-        # Start generating rooms to the east
-        direction = 1  # 1: east, -1: west
-
-
-        # While there are rooms to be created...
-        previous_room = None
-        while room_count < num_rooms:
-            if 0 <= room_count <= 9:
+def generate_rooms(self, num_rooms):
+    num_rooms = 115
+    room_count = 0
+    previous_room = None
+    while room_count < num_rooms:
+        if 0 <= room_count <= 9:
+            room_direction = "n"
+        elif 10 <= room_count <= 25:
+            if room_count % 2 != 0:
                 room_direction = "n"
-                y += 1
-                print(room_count)
-                print(direction, "North: This is x:", x)
-                print(direction,"North: This is y:", y)
-            elif 10 <= room_count <= 25:
-                if room_count % 2 != 0:
-                    room_direction = "n"
-                    y += 1
-                    print(room_count)
-                    print(direction, "North: This is x:", x)
-                    print(direction, "North: This is y:", y)
-                else:
-                    room_direction = "e"
-                    x += 1
-                    print(room_count)
-                    print(direction, "East: This is x:", x)
-                    print(direction,"East: This is y:", y)
-            elif 26 <= room_count <= 40:
-                if room_count % 2 != 0:
-                    room_direction = "s"
-                    y += -1
-                    print(room_count)
-                    print(direction, "North: This is x:", x)
-                    print(direction, "North: This is y:", y)
-                else:
-                    room_direction = "e"
-                    x += 1
-                    print(room_count)
-                    print(direction, "East: This is x:", x)
-                    print(direction,"East: This is y:", y)
-            elif 40 <= room_count <= 42:
-                if room_count % 2 != 0:
-                    room_direction = "s"
-                    y += -1
-                    print(room_count)
-                    print(direction, "North: This is x:", x)
-                    print(direction, "North: This is y:", y)
-                else:
-                    room_direction = "e"
-                    x += 1
-                    print(room_count)
-                    print(direction, "East: This is x:", x)
-                    print(direction,"East: This is y:", y)
-            elif 42 <= room_count <= 57:
-                if room_count % 2 != 0:
-                    room_direction = "n"
-                    y += 1
-                    print(room_count)
-                    print(direction, "North: This is x:", x)
-                    print(direction, "North: This is y:", y)
-                else:
-                    room_direction = "e"
-                    x += 1
-                    print(room_count)
-                    print(direction, "East: This is x:", x)
-                    print(direction,"East: This is y:", y)
-            elif  58 <= room_count <= 75:
-                if room_count % 2 != 0:
-                    room_direction = "s"
-                    y += -1
-                    print(room_count)
-                    print(direction, "North: This is x:", x)
-                    print(direction, "North: This is y:", y)
-                else:
-                    room_direction = "e"
-                    x += 1
-                    print(room_count)
-                    print(direction, "East: This is x:", x)
-                    print(direction,"East: This is y:", y)
-            elif  76 <= room_count <= 83:
-                    room_direction = "s"
-                    y += -1
-                    print(room_count)
-                    print(direction, "North: This is x:", x)
-                    print(direction, "North: This is y:", y)
-            elif  83 <= room_count <= 116:
-                    room_direction = "w"
-                    x += -1
-                    print(room_count)
-                    print(direction, "North: This is x:", x)
-                    print(direction, "North: This is y:", y)
+            else:
+                room_direction = "e"
+        elif 26 <= room_count <= 40:
+            if room_count % 2 != 0:
+                room_direction = "s"
+            else:
+                room_direction = "e"
+        elif 40 <= room_count <= 42:
+            if room_count % 2 != 0:
+                room_direction = "s"
+            else:
+                room_direction = "e"
+        elif 42 <= room_count <= 57:
+            if room_count % 2 != 0:
+                room_direction = "n"
+            else:
+                room_direction = "e"
+        elif  58 <= room_count <= 75:
+            if room_count % 2 != 0:
+                room_direction = "s"
+            else:
+                room_direction = "e"
+        elif  76 <= room_count <= 83:
+                room_direction = "s"
+        elif  83 <= room_count <= 116:
+                room_direction = "w"
+        # Create a room in the given direction
+        # room = Room(room_count, "A Generic Room", "This is a generic room.", x, y)
+        
+        #maybe add room_count as the first field to determine id?????
+        room = Room(room_count, f'Room {room_count}', f'This is room # {room_count}')
+        
+        
+        # Note that in Django, you'll need to save the room after you create it
 
-            # Create a room in the given direction
-            room = Room(room_count, "A Generic Room", "This is a generic room.", x, y)
-         
-            # Note that in Django, you'll need to save the room after you create it
+        # Save the room in the World grid
+        room.save()
+        # Connect the new room to the previous room
+        if previous_room is not None:
+            previous_room.connectRooms(room, room_direction)
 
-            # Save the room in the World grid
-            self.grid[y][x] = room
-       
+        # Update iteration variables
+        previous_room = room
+        room_count += 1
 
-            # Connect the new room to the previous room
-            if previous_room is not None:
-                previous_room.connect_rooms(room, room_direction)
-
-            # Update iteration variables
-            previous_room = room
-            room_count += 1
+players=Player.objects.all()
+for p in players:
+  p.currentRoom=0
+  p.save()
 
 
-
-    def print_rooms(self):
-        '''
-        Print the rooms in room_grid in ascii characters.
-        '''
-
-        # Add top border
-        str = "# " * ((3 + self.width * 5) // 2) + "\n"
-
-        # The console prints top to bottom but our array is arranged
-        # bottom to top.
-        #
-        # We reverse it so it draws in the right direction.
-        reverse_grid = list(self.grid) # make a copy of the list
-        reverse_grid.reverse()
-        for row in reverse_grid:
-            # PRINT NORTH CONNECTION ROW
-            str += "#"
-            for room in row:
-                if room is not None and room.n_to is not None:
-                    str += "  |  "
-                else:
-                    str += "     "
-            str += "#\n"
-            # PRINT ROOM ROW
-            str += "#"
-            for room in row:
-                if room is not None and room.w_to is not None:
-                    str += "-"
-                else:
-                    str += " "
-                if room is not None:
-                    str += f"{room.id}".zfill(3)
-                else:
-                    str += "   "
-                if room is not None and room.e_to is not None:
-                    str += "-"
-                else:
-                    str += " "
-            str += "#\n"
-            # PRINT SOUTH CONNECTION ROW
-            str += "#"
-            for room in row:
-                if room is not None and room.s_to is not None:
-                    str += "  |  "
-                else:
-                    str += "     "
-            str += "#\n"
-
-        # Add bottom border
-        str += "# " * ((3 + self.width * 5) // 2) + "\n"
-
-        # Print string
-        print(str)
+# w = World()
+# num_rooms = 115
+# width = 40
+# height = 20
+# w.generate_rooms(width, height, num_rooms)
+# w.print_rooms()
 
 
-w = World()
-num_rooms = 115
-width = 40
-height = 20
-w.generate_rooms(width, height, num_rooms)
-w.print_rooms()
-
-
-print(f"\n\nWorld\n  height: {height}\n  width: {width},\n  num_rooms: {num_rooms}\n")
+# print(f"\n\nWorld\n  height: {height}\n  width: {width},\n  num_rooms: {num_rooms}\n")
